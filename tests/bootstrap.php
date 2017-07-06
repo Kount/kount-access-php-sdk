e54fc55eb405ba9022a85e2c1b21aa0b663c24a0
preslav-anev <preslav.anev@sirma.bg>
2017-07-06 17:41:17 +0300

    KS-159 Initial commit for creating testsuites.

---
+++ tests/bootstrap.php
@@ -0,0 +1,5 @@
+<?php
+
+require __DIR__ . "/../vendor/autoload.php";
+require __DIR__ . "/../lib/kount_access_service.php";
+require __DIR__ . "/../examples/reference_implementation.php";
---
+++ tests/kount/kountaccess/unit/AccessSDKTest.php
@@ -0,0 +1,11 @@
+<?php
+
+class AccessSDKTest extends PHPUnit_Framework_TestCase {
+  const merchantId = 999999;
+  const apiKey = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiI5OTk2NjYiLCJhdWQiOiJLb3VudC4xIiwiaWF0IjoxNDk0NTM0Nzk5LCJzY3AiOnsia2EiOm51bGwsImtjIjpudWxsLCJhcGkiOmZhbHNlLCJyaXMiOnRydWV9fQ.eMmumYFpIF-d1up_mfxA5_VXBI41NSrNVe9CyhBUGck';
+  const serverUrl = 'api-sandbox02.kountaccess.com';
+  const sessionId = '8f18a81cfb6e3179ece7138ac81019aa';
+  const user = 'admin@test.com';
+  const password = 'password';
+
+}
